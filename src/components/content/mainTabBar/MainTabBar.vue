dbb28e2e0b3a68da7d027c499d73470c00be347c
shijiechn <shijiechn@aliyun.com>
2020-07-03 23:18:20 +0800

    根据tabbar划分目录结构与更新资源文件

--- src/components/content/mainTabBar/MainTabBar.vue
+++ src/components/content/mainTabBar/MainTabBar.vue
@@ -1,31 +1,31 @@
 <template>
   <tab-bar>
     <tab-bar-item path="/home" activeColor="blue">
-      <div slot="item-icon"><img src="assets/img/home.png" alt=""></div>
-      <div slot="item-icon-active"><img src="assets/img/home-active.png" alt=""></div>
+      <div slot="item-icon"><img src="~assets/img/tabbar/home.svg" alt=""></div>
+      <div slot="item-icon-active"><img src="~assets/img/tabbar/home_active.svg" alt=""></div>
       <div slot="item-name">首页</div>
     </tab-bar-item>
     <tab-bar-item path="/category" activeColor="blue">
-      <div slot="item-icon"><img src="assets/img/category.png" alt=""></div>
-      <div slot="item-icon-active"><img src="assets/img/category-active.png" alt=""></div>
+      <div slot="item-icon"><img src="~assets/img/tabbar/category.svg" alt=""></div>
+      <div slot="item-icon-active"><img src="~assets/img/tabbar/category_active.svg" alt=""></div>
       <div slot="item-name">分类</div>
     </tab-bar-item>
     <tab-bar-item path="/cart" activeColor="blue">
-      <div slot="item-icon"><img src="assets/img/cart.png" alt=""></div>
-      <div slot="item-icon-active"><img src="assets/img/cart-active.png" alt=""></div>
+      <div slot="item-icon"><img src="~assets/img/tabbar/shopcart.svg" alt=""></div>
+      <div slot="item-icon-active"><img src="~assets/img/tabbar/shopcart_active.svg" alt=""></div>
       <div slot="item-name">购物车</div>
     </tab-bar-item>
     <tab-bar-item path="profile" activeColor="blue">
-      <div slot="item-icon"><img src="assets/img/profile.png" alt=""></div>
-      <div slot="item-icon-active"><img src="assets/img/profile-active.png" alt=""></div>
+      <div slot="item-icon"><img src="~assets/img/tabbar/profile.svg" alt=""></div>
+      <div slot="item-icon-active"><img src="~assets/img/tabbar/profile_active.svg" alt=""></div>
       <div slot="item-name">我的</div>
     </tab-bar-item>
   </tab-bar>
 </template>
 
 <script>
-  import tabBar from './tabbar/TabBar'
-  import tabBarItem from './tabbar/TabBarItem'
+  import tabBar from 'components/common/tabbar/TabBar'
+  import tabBarItem from 'components/common/tabbar/TabBarItem'
 
   export default {
     name: "mainTabBar",
